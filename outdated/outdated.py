dd7d663ca34b7363b7d38b81e23e59e23cc69567
ifeola <nomorelimit11@gmail.com>
2025-01-01 08:03:22 +0100

    outdated in progress

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -12,3 +12,19 @@ months = [
   "November",
   "December"
 ]
+
+# date = "9/8/1636"
+# date_formatted = date.split("/")
+# new_date = f"{date_formatted[2]}-{int(date_formatted[0]):02}-{int(date_formatted[1]):02}"
+# print(new_date)
+
+# date = "September 8, 1636"
+# date_formatted = date.split(" ")
+# date_day = date_formatted[1][0]
+
+# for month in months:
+#     if (date_formatted[0] == month):
+#         get_month_index = months.index(date_formatted[0]) + 1
+#         print(f"{date_formatted[2]}-{int(get_month_index):02}-{int(date_day):02}")
+
+# print(date_day)
